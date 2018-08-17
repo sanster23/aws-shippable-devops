b19f88143e96d8053c085e04995d4bf147fd84bc
sanster23 <shekhawatsanjay23@gmail.com>
2018-08-17 16:28:32 +0530

    adding a ldif file

--- check_ldif.sh
+++ check_ldif.sh
@@ -2,10 +2,11 @@
 
 echo "Checking if the file is ldif or not ...."
 
-
-if [[ $NEW_FILE = "*.ldif" ]]; then
-	echo "YES this is a ldif file...."
-	echo "Adding the new user to ldap..."
-else
-	echo "This is not a ldif file"
+if [[ $changes_to_folder = "folder" ]]; then 
+	if [[ $NEW_FILE = *".ldif" ]]; then
+		echo "YES this is a ldif file...."
+		echo "Adding the new user to ldap..."
+	else
+		echo "This is not a ldif file"
+	fi
 fi
